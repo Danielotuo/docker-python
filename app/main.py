79663a0fc3973fd367c2228e66dbb8380704af66
Daniel Otuo-Acheampong <danielotuoach@gmail.com>
2023-12-14 21:12:21 -0500

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -9,11 +9,7 @@ def main():
 
     completed_process = subprocess.run(
         [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("stdout:")
     print(completed_process.stdout.strip())
-
-    print("\nstderr:")
-    print(completed_process.stderr.strip())
 
 
 if __name__ == "__main__":
